9dde641f014534bcea68d5faf9a5527c2a528d93
Dmitri Zaitsev <dmitri14@gmail.com>
2016-12-10 21:12:32 +0000

    Add list-comprehensions

---
+++ examples/list-comprehensions.js
@@ -0,0 +1,18 @@
+const { List } = require('immutable-ext')
+
+const res = List.of( x => x )
+  .ap(List([1,2,3]))
+
+console.log(res)
+
+
+const merch = () =>
+  List.of( x => y => `${x}-${y}`)
+
+  // partially apply to list of values 'x'
+  .ap(List(['T-shirt', 'sweater']))
+
+  // apply to list of values 'y' and flatten to single list
+  .ap(List(['large', 'medium', 'small']))
+
+console.log(merch())
