84c92da3fafe8dc859efd75664a99a345c9a16be
Dmitri Zaitsev <dmitri14@gmail.com>
2017-01-21 13:49:27 +0000

    Update lazy box

--- examples/11-lazy-box.js
+++ examples/11-lazy-box.js
@@ -11,7 +11,7 @@ const LazyBox = g => ({
   // compose with f from outside
   map: f => LazyBox( _ => f(g()) ),
 
-  // compose and evaluate
+  // compose and evaluate only here!
   fold: f => f(g())
 })
 
@@ -19,7 +19,7 @@ const LazyBox = g => ({
 const nextCharForNumberString = str =>
 
   // wrap 'str' first into function call
-  LazyBox( () => str )
+  LazyBox( _ => str )
 
   // every 'map' composes with new function from outside
   // without calling it
@@ -31,45 +31,8 @@ const nextCharForNumberString = str =>
   // compose again and call the function
   .fold(s => s.toLowerCase())
 
-console.log(nextCharForNumberString(' 64 '))
-
-
-// ==== Experiment
-
-// The bove LazyBox needs a function and ignores its arguments.
-// It must be called every time as LazyBox( () => val ),
-// which may not be convenient to consume
-// It may be more convenient to pass the actual value:
-
-
-// TODO === not finished yet
-
-const LazyWrapper = x => ({
-
-  wrapper: () => x,
-
-  // compose with f from outside
-  map: f => LazyWrapper( f(x) ),
-
-  // compose and evaluate
-  fold: f => f(x)
-})
-
-const nextCharForNumberString1 = str =>
-
-  // wrap 'str' first into function call
-  LazyWrapper( str )
-
-  // every 'map' composes with new function from outside
-  // without calling it
-  .map(s => s.trim())
-  .map(r => parseInt(r))
-  .map(i => i + 1)
-  .map(i => String.fromCharCode(i))
-
-  // compose again and call the function
-  .fold(s => s.toLowerCase())
-
-console.log(nextCharForNumberString1(' 64 '))
-
+console.log(
+  "nextCharForNumberString(' 64 '): ",
+  nextCharForNumberString(' 64 ')
+)
 
