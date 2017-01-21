85abbb6a4d102311b4a759880ea5d114a511276c
Dmitri Zaitsev <dmitri14@gmail.com>
2017-01-21 20:27:55 +0000

    lesson 17 rename

--- examples/17-curried.js
+++ examples/17-curried.js
@@ -1,4 +1,3 @@
-
 // divider comes first seen as customiser
 const modulo = dvr => dvd => dvd % dvr
 
@@ -9,10 +8,11 @@ console.log("Is 5 odd? ", Boolean(isOdd(5)))
 
 
 const filter = pred => xs => xs.filter(pred)
-
 const getAllOdds = filter(isOdd)
-
-console.log("The odds from [1,2,3,4] are: ", getAllOdds([1,2,3,4]))
+console.log(
+  "The odds from [1,2,3,4] are: ",
+  getAllOdds([1,2,3,4])
+)
 
 
 const replace = regex => repl => str =>
@@ -20,7 +20,6 @@ const replace = regex => repl => str =>
 
 // still accepts data 'str' as argument
 const censor = replace(/[aeiou]/g)('*')
-
 console.log(censor('hello world'))
 
 
@@ -28,6 +27,5 @@ const map = f => xs => xs.map(f)
 
 // partially apply our 'censor' to array
 const censorAll = map(censor)
-
 console.log(censorAll(['hello', 'world']))
 
