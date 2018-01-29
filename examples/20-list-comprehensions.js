a746715de0d5d53659b736b29b261aaba081a527
Dmitri Zaitsev <dmitri14@gmail.com>
2018-01-29 22:21:30 +0000

    ex20 update

--- examples/20-list-comprehensions.js
+++ examples/20-list-comprehensions.js
@@ -3,7 +3,14 @@ const { List } = require('immutable-ext')
 const res = List.of( x => x )
   .ap(List([1,2,3]))
 
-console.log(res)
+console.log(
+  `List.of( x => x ).ap(List([1,2,3])) : `,
+  res
+)
+console.log(
+  `List.of( x => x + 1 ).ap(List([1,2,3])) : `,
+  List.of( x => x + 1 ).ap(List([1,2,3]))
+)
 
 
 const merch = _ =>
@@ -15,4 +22,7 @@ const merch = _ =>
   // apply to list of values 'y' and flatten to single list
   .ap(List(['large', 'medium', 'small']))
 
-console.log(merch())
+console.log(
+  "List.of( x => y => `${x}-${y}`).ap(List(['T-shirt', 'sweater'])).ap(List(['large', 'medium', 'small'])) : ",
+  merch()
+)
